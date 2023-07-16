dbb765c2025af1705ffd24669df7b1da32716b80
Jayanth Mahadesh <107387824+jayanthmahadesh@users.noreply.github.com>
2023-07-16 14:54:03 +0530

    Update tries.cpp

--- tries/tries.cpp
+++ tries/tries.cpp
@@ -1,1 +1,65 @@
-//code here
+struct Node{
+    Node* list[26];
+    bool flag=false;
+    bool containskey(char ch){
+        return list[ch-'a']!=NULL;
+    }
+    void put(char ch,Node* node)
+    {
+        list[ch-'a']=node;
+    }
+    Node* get(char ch){
+        return list[ch-'a'];
+    }
+    void setend(){
+        flag=true;
+    }
+    bool isend(){
+        return flag;
+    }
+};
+class Trie {
+    private: Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        int i;
+        for(i=0;i<word.length();i++){
+            if(!node->containskey(word[i])){
+                node->put(word[i],new Node());
+            }
+        node = node->get(word[i]);
+        }
+        node->setend();
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        for(int i=0;i<word.size();i++){
+            if(!node->containskey(word[i]))return false;
+            node = node->get(word[i]);
+        }
+        return node->isend();
+    }
+    
+    bool startsWith(string word) {
+        Node* node = root;
+        for(int i=0;i<word.size();i++){
+            if(!node->containskey(word[i]))return false;
+            node=node->get(word[i]);
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
